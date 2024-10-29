2b16ddce6e26a17c205f297318969b256915d522
Niko Bagaric <niko.bagaric275@gmail.com>
2024-10-29 22:21:37 +0100

    fix: fixed routes

--- app/controllers/recipes_controller.ts
+++ app/controllers/recipes_controller.ts
@@ -30,7 +30,6 @@ export default class RecipesController {
    * @description Gets one recipe by id
    * @responseBody 200
    * @responseBody 404
-   * @requestBody {"id": "number"}
    */
   public async show({ response, params }: HttpContext) {
     const recipe = await Recipe.findOrFail(params.id)
@@ -43,7 +42,7 @@ export default class RecipesController {
    * @description Save a recipe
    * @responseBody 201
    * @responseBody 404
-   * @requestBody {"title": "string", "description": "string", "difficulty": "1-5", "image": "file", "ingredients": "list"}
+   * @requestBody {"title": "string", "description": "string", "difficulty": "1-5", "ingredients": "list"}
    */
   public async store({ request, response }: HttpContext) {
     const data: RecipeData = request.only(['title', 'description', 'difficulty', 'ingredients'])
@@ -73,7 +72,7 @@ export default class RecipesController {
    * @description Update a recipe
    * @responseBody 200
    * @responseBody 404
-   * @requestBody {"title": "string", "description": "string", "difficulty": "1-5", "image_url": "string"}
+   * @requestBody {"title": "string", "description": "string", "difficulty": "1-5"}
    */
   public async update({ request, response, params }: HttpContext) {
     const recipe = await Recipe.findOrFail(params.id)
@@ -102,7 +101,6 @@ export default class RecipesController {
   }
 
   /**
-   * @destroy
    * @operationId destroyRecipe
    * @responseBody 204
    * @responseBody 404
--- start/routes.ts
+++ start/routes.ts
@@ -8,12 +8,15 @@
 */
 
 const AuthController = () => import('#controllers/auth_controller')
+const UsersController = () => import('#controllers/users_controller')
+const RecipesController = () => import('#controllers/recipes_controller')
 
 import router from '@adonisjs/core/services/router'
 
 // Auto-swagger import:
 import AutoSwagger from "adonis-autoswagger";
 import swagger from "#config/swagger";
+
 
 router.get("/swagger", async () => {
   return AutoSwagger.default.docs(router.toJSON(), swagger)
@@ -29,6 +32,23 @@ router.group(() => {
   router.post('/login', [AuthController, 'login'])
   router.post('/logout', [AuthController, 'logout'])
 }).prefix('auth')
+
+
+router.group(() => {
+  router.get('/user', [UsersController, 'index'])
+  router.get('/user/:id', [UsersController, 'show'])
+  router.post('/user', [UsersController, 'store'])
+  router.put('/user/:id', [UsersController, 'update'])
+  router.delete('/user/:id', [UsersController, 'destroy'])
+}).prefix('users')
+
+router.group(() => {
+  router.get('/recipe', [RecipesController, 'index'])
+  router.get('/recipe/:id', [RecipesController, 'show'])
+  router.post('/recipe', [RecipesController, 'store'])
+  router.put('/recipe/:id', [RecipesController, 'update'])
+  router.delete('/recipe/:id', [RecipesController, 'destroy'])
+}).prefix('recipes')
 
 router.get('/', async () => {
   return { welcome: "cookit api indev, go to /swagger or /docs"}
