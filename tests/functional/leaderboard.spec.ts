481c310bc8ae0e6a8e44a02fe19a775bb9de6b97
Niko Bagaric <niko.bagaric275@gmail.com>
2024-11-11 12:15:31 +0100

    feat: added leaderboard tests

---
+++ tests/functional/leaderboard.spec.ts
@@ -0,0 +1,26 @@
+import { test } from '@japa/runner'
+import UserProfile from '#models/user_profile'
+
+test.group('Leaderboard', () => {
+  test('should get leaderboard (empty)', async ({ client }) => {
+    const response = await client.get('/leaderboards/leaderboard')
+    response.assertStatus(200)
+    response.assertBodyContains([])
+  })
+
+  test('should get leaderboard after update', async ({ client }) => {
+    const userProfile = await UserProfile.create({
+      userId: 1,
+      imageUrl: "test.png",
+      bio: "testing bio",
+    })
+    await client.put(`/leaderboards/leaderboard/${userProfile.id}`).field('points', 200)
+    const response = await client.get('/leaderboards/leaderboard')
+
+    response.assertStatus(200)
+    response.assertBodyContains([{
+      "points": 200,
+      "userId": userProfile.userId
+    }])
+  })
+})
